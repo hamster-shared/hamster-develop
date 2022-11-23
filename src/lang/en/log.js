a838ab932238f44a38d11f46edd7cf465460c3b4
wangshuqin <shuqin.wang@newtouch.com>
2022-11-23 11:06:42 +0800

    en-zh

--- src/lang/en/log.js
+++ src/lang/en/log.js
@@ -3,4 +3,8 @@ export default {
   viewAllLogs: "View the complete log",
   full: "Full",
   restore: "Restore",
+  status: 'Status',
+  executionTime: "Execution Time",
+  totalDuration: "Total duration",
+  executionProcess: "Execution Process",
 };
