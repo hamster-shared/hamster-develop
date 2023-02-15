7e3219edd2bea258220853e9fd8b66d866f71a85
mohaijiang <mohaijiang110@163.com>
2023-02-15 14:58:33 +0800

    contract table modify
    contract table modify

--- pkg/db/contract.go
+++ pkg/db/contract.go
@@ -18,15 +18,20 @@ type Contract struct {
 	AbiInfo          string         `json:"abiInfo"`
 	ByteCode         string         `json:"byteCode"`
 	CreateTime       time.Time      `gorm:"column:create_time;default:current_timestamp" json:"createTime"`
+	Type             uint           `json:"type"` // see #consts.ProjectFrameType
 }
 
 type ContractDeploy struct {
-	Id         uint      `gorm:"primaryKey" json:"id"`
-	ContractId uint      `json:"contractId"`
-	ProjectId  uuid.UUID `json:"projectId"`
-	Version    string    `json:"version"`
-	DeployTime time.Time `gorm:"column:deploy_time;default:current_timestamp" json:"deployTime"`
-	Network    string    `json:"network"`
-	Address    string    `json:"address"`
-	CreateTime time.Time `gorm:"column:create_time;default:current_timestamp" json:"createTime"`
+	Id            uint      `gorm:"primaryKey" json:"id"`
+	ContractId    uint      `json:"contractId"`
+	ProjectId     uuid.UUID `json:"projectId"`
+	Version       string    `json:"version"`
+	DeployTime    time.Time `gorm:"column:deploy_time;default:current_timestamp" json:"deployTime"`
+	Network       string    `json:"network"`
+	Address       string    `json:"address"`
+	CreateTime    time.Time `gorm:"column:create_time;default:current_timestamp" json:"createTime"`
+	Type          uint      `json:"type"` // see #consts.ProjectFrameType
+	DeclareTxHash string    `json:"declareTxHash"`
+	DeployTxHash  string    `json:"deployTxHash"`
+	Status        string    `json:"status"` // 1: deploying, 2: success , 3: fail
 }
---
+++ pkg/db/migration/v8__contract_deploy.sql
@@ -0,0 +1,15 @@
+alter table t_contract
+    add type int(1) default '1' not null comment '合约类型： 1:evm, 4:starknet';
+
+alter table t_contract_deploy
+    add type int(1) default '1' not null comment '合约类型： 1:evm, 4:starknet';
+
+alter table t_contract_deploy
+    add declare_tx_hash char(72) null comment 'declare 交易hash(starknet专用)';
+
+alter table t_contract_deploy
+    add deploy_tx_hash char(72) null comment '部署hash';
+
+alter table t_contract_deploy
+    add status int(1) default 2 null comment ' 1: deploying, 2: success , 3: fail';
+
