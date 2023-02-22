f4e7948929b7eec1b60349efafb6e237592c2188
sunjianguo <1275596256@qq.com>
2023-02-22 16:14:09 +0800

    delete frontend and change pipeline.yml

--- frontend/src/lang/zh/component.js
+++
@@ -1,3 +0,0 @@
-export default {
-  back: "返回",
-};
--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./App.vue", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
