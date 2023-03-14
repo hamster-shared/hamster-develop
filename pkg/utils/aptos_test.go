2bfbf4b5b610b84ad86177ca04c5fbf349fa9e09
jlvihv <vihv@qq.com>
2023-03-14 18:38:31 +0800

    add some function about aptos

---
+++ pkg/utils/aptos_test.go
@@ -0,0 +1,157 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseMoveToml(t *testing.T) {
+	type args struct {
+		tomlPath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *MoveToml
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				tomlPath: "./test_data/test.toml",
+			},
+			want: &MoveToml{
+				Package: Package{
+					Name:    "mokshya-staking",
+					Version: "1.0.0",
+				},
+				Addresses: map[string]string{
+					"Std":            "0x1",
+					"aptos_std":      "0x1",
+					"mokshyastaking": "_",
+				},
+				Dependencies: map[string]any{
+					"AptosFramework": map[string]any{
+						"local": "../../aptos-core/aptos-move/framework/aptos-framework",
+					},
+					"AptosToken": map[string]any{
+						"local": "../../aptos-core/aptos-move/framework/aptos-token",
+					},
+					"AptosStdlib": map[string]any{
+						"local": "../../aptos-core/aptos-move/framework/aptos-stdlib",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseMoveToml(tt.args.tomlPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseMoveToml() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseMoveToml() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFillKeyValueToMoveTomlForce(t *testing.T) {
+	type args struct {
+		tomlPath       string
+		keyValueString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				tomlPath:       "./test_data/test.toml",
+				keyValueString: "Std=0x2",
+			},
+			wantErr: false,
+		},
+		{
+			name: "recover",
+			args: args{
+				tomlPath:       "./test_data/test.toml",
+				keyValueString: "Std=0x1",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := FillKeyValueToMoveTomlForce(tt.args.tomlPath, tt.args.keyValueString); (err != nil) != tt.wantErr {
+				t.Errorf("FillKeyValueToMoveTomlForce() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFillKeyValueToMoveToml(t *testing.T) {
+	type args struct {
+		tomlPath       string
+		keyValueString string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				tomlPath:       "./test_data/test.toml",
+				keyValueString: "Std=0x2",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := FillKeyValueToMoveToml(tt.args.tomlPath, tt.args.keyValueString); (err != nil) != tt.wantErr {
+				t.Errorf("FillKeyValueToMoveToml() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFileToHexString(t *testing.T) {
+	type args struct {
+		filePath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				filePath: "./test_data/test.txt",
+			},
+			want:    "68656c6c6f20776f726c64",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FileToHexString(tt.args.filePath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FileToHexString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FileToHexString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- pkg/utils/file_utils_test.go
+++ pkg/utils/file_utils_test.go
@@ -20,3 +20,56 @@ func Test_GetRedundantPath(t *testing.T) {
 		}
 	}
 }
+
+func Test_readStringFromFile(t *testing.T) {
+	type args struct {
+		filePath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				filePath: "./test_data/test.toml",
+			},
+			want: `[package]
+name = 'mokshya-staking'
+version = '1.0.0'
+
+
+[addresses]
+mokshyastaking = "_"
+Std = "0x1"
+aptos_std = "0x1"
+
+[dependencies]
+AptosFramework = { local = "../../aptos-core/aptos-move/framework/aptos-framework"}
+AptosToken = { local = "../../aptos-core/aptos-move/framework/aptos-token" }
+AptosStdlib = { local = "../../aptos-core/aptos-move/framework/aptos-stdlib" }`,
+			wantErr: false,
+		}, {
+			name: "test file not exist",
+			args: args{
+				filePath: "./test_data/testtest.toml",
+			},
+			want:    ``,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := readStringFromFile(tt.args.filePath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("readStringFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("readStringFromFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
