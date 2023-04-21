22de10ec26be98777d965b8b31cd41bdb7f1980b
sunjianguo <1275596256@qq.com>
2023-04-21 17:01:23 +0800

    add meta scan model

---
+++ pkg/service/meta_scan_mode.go
@@ -0,0 +1,31 @@
+package service
+
+type SecurityAnalyzerResponse struct {
+	Success bool     `json:"success"`
+	Error   string   `json:"error"`
+	Results []string `json:"results"`
+}
+
+type SecurityAnalyzerResult struct {
+	Mwe           MalwareWorkflowEngine `json:"mwe"`
+	ShowTitle     string                `json:"show_title"`
+	AffectedFiles []AffectedFile        `json:"affected_files"`
+}
+
+type AffectedFile struct {
+	Filepath         string `json:"filepath"`
+	FilepathRelative string `json:"filepath_relative"`
+	Hightlights      []int  `json:"hightlights"`
+	LineStart        int    `json:"line_start"`
+	LineEnd          int    `json:"line_end"`
+}
+
+type MalwareWorkflowEngine struct {
+	Id             string `json:"id"`
+	Code           string `json:"code"`
+	Severity       string `json:"severity"`
+	Title          string `json:"title"`
+	Confidence     string `json:"confidence"`
+	Description    string `json:"description"`
+	Recommendation string `json:"recommendation"`
+}
