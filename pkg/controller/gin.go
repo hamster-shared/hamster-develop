ed3a3b620fa0352c9566a627d83f3db5a0bd2da3
sunjianguo <1275596256@qq.com>
2023-05-04 15:20:58 +0800

    query contract file content

--- pkg/controller/gin.go
+++ pkg/controller/gin.go
@@ -107,6 +107,7 @@ func (h *HttpServer) StartHttpServer() {
 	api.GET("/workflows/:id/detail/:detailId/report/overview", h.handlerServer.workflowReportOverview)
 	api.GET("/report/:id", h.handlerServer.reportDetail)
 	api.GET("/metascan/file/*key", h.handlerServer.metaScanFile)
+	api.GET("/project/:id/contract/:name/content", h.handlerServer.contractFileContent)
 	// frontend reports
 	api.GET("/workflows/:id/detail/:detailId/frontend/report", h.handlerServer.workflowFrontendReports)
 	//workflow frontend packages
