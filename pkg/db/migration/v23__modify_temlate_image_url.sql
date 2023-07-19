42fde46e65a86f74df49506c6e27fdcb011aba91
mohaijiang <mohaijiang110@163.com>
2023-07-19 18:27:24 +0800

    modify container images

---
+++ pkg/db/migration/v23__modify_temlate_image_url.sql
@@ -0,0 +1,4 @@
+update  t_template set image = 'https://g.alpha.hamsternet.io/ipfs/QmdhtgKNuQn2aqkdTn4DxRQidSLmmtggtpaBgh8vuyVjxd' where image = 'https://develop-images.api.hamsternet.io/vue.png';
+update  t_template set image = 'https://g.alpha.hamsternet.io/ipfs/QmUcXKszQxwT21dnqf67vFxzBgFwT8iWEsxExeGA1oFf6N' where image = 'https://develop-images.api.hamsternet.io/react.png';
+update  t_template set image = 'https://g.alpha.hamsternet.io/ipfs/QmdjD6f7SfSHUydAjA1bwagQ9FPXfU8fMHaZvypNteRbKy' where image = 'https://develop-images.api.hamsternet.io/nuxt.png';
+update  t_template set image = 'https://g.alpha.hamsternet.io/ipfs/QmfCkFjMvdnZJin9SwJARk6Ro2kkyLzRfLX5rKuM8coGtc' where image = 'https://develop-images.api.hamsternet.io/next.png';
