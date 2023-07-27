b1532c31f47e41d2f9715c1645bd72272f664991
sunjianguo <1275596256@qq.com>
2023-07-27 13:52:06 +0800

    add icp canister list

--- pkg/vo/icp_dfx_data.go
+++ pkg/vo/icp_dfx_data.go
@@ -1,7 +1,22 @@
 package vo
 
+import (
+	"database/sql"
+	"github.com/hamster-shared/hamster-develop/pkg/db"
+)
+
 type IcpDfxDataVo struct {
 	Id        int    `json:"id"`
 	ProjectId string `json:"projectId"`
 	DfxData   string `json:"dfxData"`
+}
+
+type IcpCanisterVo struct {
+	Id           int               `json:"id"`
+	CanisterId   string            `json:"canisterId"`
+	CanisterName string            `json:"canisterName"`
+	Cycles       string            ` json:"cycles"`
+	Status       db.CanisterStatus `json:"status"`
+	CreateTime   sql.NullTime      `json:"createTime"`
+	UpdateTime   sql.NullTime      `json:"updateTime"`
 }
