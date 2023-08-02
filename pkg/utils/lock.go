2d90082f3f5b722435043bb67a614e4490e8a990
mohaijiang <mohaijiang110@163.com>
2023-08-02 14:44:38 +0800

    set lock.go  ICP_LOCK_KEY

--- pkg/utils/lock.go
+++ pkg/utils/lock.go
@@ -2,15 +2,16 @@ package utils
 
 import (
 	"github.com/hamster-shared/aline-engine/logger"
+	"github.com/werf/kubedog/pkg/kube"
 	"github.com/werf/lockgate"
 	"github.com/werf/lockgate/pkg/distributed_locker"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"os"
-	"github.com/werf/kubedog/pkg/kube"
+	"time"
 )
 
 const (
-	ICP_LOCK_KEY = "icp_lock_resource"
+	ICP_LOCK_KEY = "icp-lock"
 	NAMESPACE    = "hamster"
 )
 
@@ -37,17 +38,15 @@ func Lock() (*lockgate.LockHandle, error) {
 	// Case 1: simple blocking lock
 	logger.Info("try to lock:", ICP_LOCK_KEY)
 
-	//_, lock, err := locker.Acquire(ICP_LOCK_KEY, lockgate.AcquireOptions{Shared: false, Timeout: 30 * time.Second})
-	//if err != nil {
-	//	logger.Error(os.Stderr, "ERROR: failed to lock %s: %s\n", ICP_LOCK_KEY, err)
-	//	return nil, err
-	//}
-	//return &lock, err
-	return nil,nil,
+	_, lock, err := locker.Acquire(ICP_LOCK_KEY, lockgate.AcquireOptions{Shared: false, Timeout: 30 * time.Second})
+	if err != nil {
+		logger.Error(os.Stderr, "ERROR: failed to lock %s: %s\n", ICP_LOCK_KEY, err)
+		return nil, err
+	}
+	return &lock, err
 }
 
 func Unlock(lock *lockgate.LockHandle) error {
-	//logger.Info("try to release lock:", ICP_LOCK_KEY)
-	//return locker.Release(*lock)
-	return nil
+	logger.Info("try to release lock:", ICP_LOCK_KEY)
+	return locker.Release(*lock)
 }
