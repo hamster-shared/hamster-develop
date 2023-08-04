c6188472a00e674c0b0acc13b6e4820b769d1454
sunjianguo <1275596256@qq.com>
2023-08-04 10:45:41 +0800

    add icp react template

--- pkg/db/migration/v25__create_icp_data.sql
+++ pkg/db/migration/v25__create_icp_data.sql
@@ -340,3 +340,128 @@ create table t_icp_canister (
     create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
     update_time timestamp NULL DEFAULT CURRENT_TIMESTAMP comment '更新时间'
 );
+
+insert into t_template (
+    id,
+    template_type_id,
+    name,
+    description,
+    audited,
+    last_version,
+    whether_display,
+    language_type,
+    logo,
+    image,
+    deploy_type
+) values (
+             53,
+             5,
+             'icp_react',
+             'A client-side React appcreated with create-react-app.',
+             1,
+             '1.0.0',
+             1,
+             0,
+             'https://reactjs.org/favicon.ico',
+             'https://g.alpha.hamsternet.io/ipfs/QmUcXKszQxwT21dnqf67vFxzBgFwT8iWEsxExeGA1oFf6N',
+             3
+         );
+INSERT INTO t_frontend_template_detail (
+    id,
+    template_id,
+    name,
+    examples,
+    author,
+    repository_url,
+    repository_name,
+    branch,
+    version,
+    audited,
+    template_type,
+    show_url,
+    description
+) values (
+             6,
+             53,
+             'icp_react',
+             '',
+             'hamster-template',
+             'https://github.com/hamster-template/icp_react.git',
+             'icp_react',
+             'main',
+             '1.0.0',
+             1,
+             1,
+             'https://g.alpha.hamsternet.io/ipfs/QmUcXKszQxwT21dnqf67vFxzBgFwT8iWEsxExeGA1oFf6N',
+             '# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can''t go back!**
+
+If you aren''t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you''re on your own.
+
+You don''t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn''t feel obligated to use this feature. However we understand that this tool wouldn''t be useful if you couldn''t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+'
+         );
