f7983fb24b3266db014a999df61881cf1c95d791
mohaijiang <mohaijiang110@163.com>
2023-08-29 18:14:39 +0800

    fix sql error

--- pkg/db/migration/v28__icp_deploy.sql
+++ pkg/db/migration/v28__icp_deploy.sql
@@ -27,29 +27,6 @@ create table t_backend_package
 )
     comment 'backend deploy  package table';
 
-
-create table t_contract_deploy
-(
-    id              int auto_increment
-        primary key,
-    contract_id     int                                 null comment '合约id',
-    project_id      char(36)                            null comment '项目ID',
-    version         varchar(50)                         null comment '部署版本',
-    deploy_time     timestamp                           null comment '部署时间',
-    network         varchar(50)                         null comment '部署网络',
-    address         varchar(100)                        null comment '部署地址',
-    create_time     timestamp default CURRENT_TIMESTAMP null comment '创建时间',
-    type            int       default 1                 not null comment '合约类型： 1:evm, 4:starknet',
-    declare_tx_hash char(72)                            null comment 'declare 交易hash(starknet专用)',
-    deploy_tx_hash  char(72)                            null comment '部署hash',
-    status          int       default 2                 null comment ' 1: deploying, 2: success , 3: fail',
-    abi_info        mediumtext                          null comment 'abi信息',
-    name            varchar(50)                         null comment 'ICP部署， 罐名'
-)
-    comment '合约部署表';
-
-
-
 create table t_backend_deploy
 (
     id                 int auto_increment
